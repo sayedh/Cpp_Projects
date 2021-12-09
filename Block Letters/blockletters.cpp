abf0c82fa89830d53750e20b076fe5b57918d347
sayedh <30685241+sayedh@users.noreply.github.com>
2021-12-09 12:02:30 -0800

    Add files via upload

---
+++ Block Letters/blockletters.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+						  
+	cout << "  SSS   M   M  H   H\n";
+	cout << " S   S  MM MM  H   H\n";
+	cout << " S      MM MM  H   H\n";
+	cout << "  SSS   M M M  HHHHH\n";
+	cout << "     S  M   M  H   H\n";
+	cout << " S   S  M   M  H   H\n";
+	cout << "  SSS   M   M  H   H\n";
+	
+	cout << " \n";
+	system("pause");
+
+}
---
+++ Currency/currency.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int main() {
+
+	double p, q, c, dollars;
+
+	cout << "Enter number of Pesos: ";
+	cin >> p;
+
+	cout << "Enter number of Guatemalan Quetzals: ";
+	cin >> q;
+
+	cout << "Enter number of Salvadoran Colons: ";
+	cin >> c;
+
+	dollars = 0.049 * p + 0.1305 * q + 0.1144 * c;
+
+	cout << "Total USD = $" << dollars << "\n";
+
+	cout << " \n";
+	system("pause");
+}
